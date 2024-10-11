b773a4a24e97fd58fee5c36e3aafc427e11ef059
ptai82067 <ptai82067@gmail.com>
2024-10-11 16:04:10 +0700

    new

--- app/src/main/java/thi/tai64132083/listv1/MainActivity.java
+++ app/src/main/java/thi/tai64132083/listv1/MainActivity.java
@@ -1,15 +1,11 @@
-package com.example.listview_coban;
+package thi.tai64132083.listv1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -21,11 +17,11 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-ArrayList<String> list;
-ArrayAdapter<String> myAdapter;
-ListView lv;
-Button btnThem;
-EditText edtThem;
+    // khai bao bien
+    String[] listNhac = {"Hỏi thăm nhau","Lá xa lìa cành", "Đùa hơi quá", "Nắng ấm xa dần", "Lạc trôi"};
+    ListView lv;
+    ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,33 +32,16 @@ EditText edtThem;
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        list = new ArrayList<>();
-        list.add("Hỏi Thăm Nhau");
-        list.add("Lá Xa Lìa Cành");
-        list.add("Trách Ai Vô Tình");
-        list.add("Hỏi Vợ Ngoại Thành");
-        list.add("Nắng Ấm Xa Dần");
-        list.add("Lạc Trôi");
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,listNhac);
         lv = findViewById(R.id.lv);
-        btnThem = findViewById(R.id.btnThem);
-        edtThem = findViewById(R.id.edtThem);
-        myAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,list);
-        lv.setAdapter(myAdapter);
-
-        btnThem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String tenBaiHat = edtThem.getText().toString();
-                list.add(tenBaiHat);
-                myAdapter.notifyDataSetChanged();
-            }
-        });
+        lv.setAdapter(adapter);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(), list.get(i), Toast.LENGTH_SHORT).show();
+                Intent myIntent = new Intent(MainActivity.this, SubActivity.class);
+                myIntent.putExtra("name",listNhac[i]);
+                startActivity(myIntent);
             }
         });
     }
-
 }
