7180da2ebe5cefa3d58b7e4d8fb2ac9a94de479a
ptai82067 <ptai82067@gmail.com>
2024-12-03 14:45:04 +0700

    _______BTH1_______

--- BTH1/app/src/main/java/th/phamphuoctai/basicgui_simplemath/MainActivity.java
+++ BTH1/app/src/main/java/th/phamphuoctai/basicgui_simplemath/MainActivity.java
@@ -4,70 +4,62 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-EditText edtA, edtB, edtKetQua;
-Button btnCong, btnTru, btnChia, btnNhan;
+    EditText edtA, edtB, edtKetQua;
+    Button btnKQ;
+    private RadioGroup radioGroup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Ánh xạ các view
         edtA = findViewById(R.id.edtA);
         edtB = findViewById(R.id.edtB);
         edtKetQua = findViewById(R.id.edtKetQua);
-        btnCong = findViewById(R.id.btnCong);
-        btnTru = findViewById(R.id.btnTru);
-        btnNhan = findViewById(R.id.btnNhan);
-        btnChia = findViewById(R.id.btnChia);
-        btnCong.setOnClickListener(new View.OnClickListener() {
+        radioGroup = findViewById(R.id.radioGroup);
+        btnKQ = findViewById(R.id.btnKQ);
+
+        // Gán sự kiện click cho nút Kết quả
+        btnKQ.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String kq ="";
+                // Kiểm tra đầu vào
+                if (edtA.getText().toString().isEmpty() || edtB.getText().toString().isEmpty()) {
+                    edtKetQua.setText("Please input both numbers!");
+                    return;
+                }
+
+                // Kiểm tra phép toán đã được chọn hay chưa
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                if (selectedId == -1) {
+                    edtKetQua.setText("Please select an operation!");
+                    return;
+                }
+
+                // Đọc giá trị nhập vào
                 int a = Integer.parseInt(edtA.getText().toString());
                 int b = Integer.parseInt(edtB.getText().toString());
-                kq = String.valueOf(a+b);
-                edtKetQua.setText(kq);
-            }
-        });
-        btnTru.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String kq ="";
-                int a = Integer.parseInt(edtA.getText().toString());
-                int b = Integer.parseInt(edtB.getText().toString());
-                kq = String.valueOf(a-b);
-                edtKetQua.setText(kq);
-            }
-        });
-        btnNhan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String kq ="";
-                int a = Integer.parseInt(edtA.getText().toString());
-                int b = Integer.parseInt(edtB.getText().toString());
-                kq = String.valueOf(a*b);
-                edtKetQua.setText(kq);
-            }
-        });
-        btnChia.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String kq ="";
-                int a = Integer.parseInt(edtA.getText().toString());
-                int b = Integer.parseInt(edtB.getText().toString());
-                kq = String.valueOf(a/b);
+                String kq = "";
+
+                // Thực hiện phép toán
+                // Thực hiện phép toán bằng if-else
+                if (selectedId == R.id.radioOption1) { // Phép cộng
+                    kq = String.valueOf(a + b);
+                } else if (selectedId == R.id.radioOption2) { // Phép trừ
+                    kq = String.valueOf(a - b);
+                } else if (selectedId == R.id.radioOption3) { // Phép nhân
+                    kq = String.valueOf(a * b);
+                } else if (selectedId == R.id.radioOption4) { // Phép chia
+                    kq = b != 0 ? String.valueOf((float) a / b) : "Error: Divide by Zero";
+                }
+
+                // Hiển thị kết quả
                 edtKetQua.setText(kq);
             }
         });
